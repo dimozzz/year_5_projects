8af3190b37b922d86701576a7f16c644d38a7479
Ivan Sorokin <Ivan.Sorokin@transas.com>
2010-05-29 15:41:35 +0400

    Допилил сервер
    Если из вызова клиенты бросается исключение килять клиента
    Корба делает входящии вызовы в потоках из пула, поставил критическую секцию

--- chat/temp_version/src/chat/server_i.h
+++ chat/temp_version/src/chat/server_i.h
@@ -5,6 +5,8 @@
 #include <utility>
 #include <vector>
 
+#include <boost/thread.hpp>
+
 typedef std::pair<Chat::User_ptr, std::string> user_name_t;
 struct user_is
 {
@@ -34,74 +36,87 @@ struct name_is
     std::string n_;
 };
 
-struct vector_bimap
+typedef std::vector<user_name_t> user_name_vec;
+
+bool insert(user_name_vec & v, Chat::User_ptr u, std::string const & name)
 {
-    typedef std::vector<user_name_t> user_name_vec;
+   if (std::find_if(v.begin(), v.end(), user_is(u)) != v.end())
+      return false;
+   if (std::find_if(v.begin(), v.end(), name_is(name)) != v.end())
+      return false;
 
-    bool insert(Chat::User_ptr u, std::string const & name)
-    {
-        if (std::find_if(v_.begin(), v_.end(), user_is(u)) != v_.end())
-            return false;
-        if (std::find_if(v_.begin(), v_.end(), name_is(name)) != v_.end())
-            return false;
+   v.push_back(user_name_t(Chat::User::_duplicate(u), name));
+   return true;
+}
 
-        v_.push_back(user_name_t(Chat::User::_duplicate(u), name));
-        return true;
-    }
+void erase(user_name_vec & v, Chat::User_ptr u)
+{
+   user_name_vec::iterator i = std::find_if(v.begin(), v.end(), user_is(u));
+   if (i == v.end())
+   {
+      std::cerr << "vector_bimap::erase: unknown user" << std::endl;
+      return;
+   }
 
-    void erase(Chat::User_ptr u)
-    {
-        user_name_vec::iterator i = std::find_if(v_.begin(), v_.end(), user_is(u));
-        if (i == v_.end())
-        {
-            std::cerr << "vector_bimap::erase: unknown user" << std::endl;
-            return;
-        }
+   assert(std::find_if(i + 1, v.end(), user_is(u)) == v.end());
 
-        assert(std::find_if(i + 1, v_.end(), user_is(u)) == v_.end());
+   v.erase(i);
+}
 
-        v_.erase(i);
-    }
-
-    user_name_vec::iterator find(Chat::User_ptr u)
-    {
-        return std::find_if(v_.begin(), v_.end(), user_is(u));
-    }
-
-    // TODO: release references
-    user_name_vec v_;
-};
+user_name_vec::iterator find(user_name_vec & v, Chat::User_ptr u)
+{
+   return std::find_if(v.begin(), v.end(), user_is(u));
+}
 
 struct Server_i : POA_Chat::Server
 {
+    typedef boost::lock_guard<boost::mutex> lock_t;
+
     Server_i()
     {}
 
     ::CORBA::Boolean _cxx_register(::Chat::User_ptr u, const char* name)
     {
-        return v_.insert(u, name);
+        lock_t lock(m_);
+        return insert(v_, u, name);
     }
 
     void quit(::Chat::User_ptr u)
     {
-        v_.erase(u);
+        lock_t lock(m_);
+        erase(v_, u);
     }
 
     void send(::Chat::User_ptr u, const char* message)
     {
-        vector_bimap::user_name_vec::iterator ui = v_.find(u);
-        if (ui == v_.v_.end())
+        lock_t lock(m_);
+        std::string u_name;
+
         {
-            std::cerr << "send: unregistered user: " << u << std::endl;
-            return;
+            user_name_vec::iterator ui = find(v_, u);
+            if (ui == v_.end())
+                u_name = "<unregistered user>";
+            else
+                u_name = ui->second;
         }
 
-        for (vector_bimap::user_name_vec::const_iterator i = v_.v_.begin();
-             i != v_.v_.end();
-             ++i)
-           i->first->receive(ui->second.c_str(), message);
+        user_name_vec::const_iterator i = v_.begin();
+        while (i != v_.end())
+        {
+            try
+            {
+               i->first->receive(u_name.c_str(), message);
+               ++i;
+            }
+            catch (CORBA::Exception const & e)
+            {
+               std::cerr << "Caught a CORBA::" << e._name() << " in send (kick user \"" << i->second << "\")." << std::endl;
+               i = v_.erase(i);
+            }
+        }
     }
 
 private:
-    vector_bimap v_;
+    user_name_vec v_;
+    boost::mutex m_;
 };
