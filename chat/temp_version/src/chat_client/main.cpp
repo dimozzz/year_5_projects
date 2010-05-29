88f406d77454126785e83729f3e985b94e6fd116
Ivan Sorokin <Ivan.Sorokin@transas.com>
2010-05-29 15:28:44 +0400

    Проверка на количество переданных аргументов в клиенте, чтобы он не падал, когда его запускают без аргументов

--- chat/temp_version/src/chat_client/main.cpp
+++ chat/temp_version/src/chat_client/main.cpp
@@ -18,6 +18,9 @@ struct User_i : POA_Chat::User
 
 int main( int argc, char** argv )
 {
+    if (argc != 2)
+       return 1;
+
     try {
         CORBA::ORB_var orb = CORBA::ORB_init(argc, argv);
 
