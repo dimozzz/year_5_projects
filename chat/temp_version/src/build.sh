e2531b2debac41ed1ba6d55009c841c4035d38fa
Ivan Sorokin <sorokin@rain.ifmo.ru>
2010-06-01 00:36:26 +0400

    Не ругаться, если каталог bin уже существует

--- chat/temp_version/src/build.sh
+++ chat/temp_version/src/build.sh
@@ -1,3 +1,7 @@
-mkdir ../bin
+if ! [ -d ../bin ]
+then
+   mkdir ../bin
+fi
+
 g++ -ggdb `pkg-config --cflags --libs omniORB4` -l boost_thread -o ../bin/chat chat/main.cpp idl/chatSK.cc
 g++ -ggdb `pkg-config --cflags --libs omniORB4` -o ../bin/chat_client chat_client/main.cpp idl/chatSK.cc
