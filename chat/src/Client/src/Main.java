3ef776677f41560359f7ac7fe510df8a7fbdbebb
dimozzz <dimoz_88@rambler.ru>
2010-06-27 23:35:45 +0400

    work with utf8 version

--- chat/src/Client/src/Main.java
+++ chat/src/Client/src/Main.java
@@ -78,7 +78,7 @@ public class Main {
 		p.setProperty("com.sun.CORBA.codeset.charsets", "0x05010001"); // UTF-8
 		p.setProperty("com.sun.CORBA.codeset.wcharsets", "0x00010109, 0x05010001"); // UTF-16, UTF-8
 
-        final ORB orb = ORB.init(args, null);
+        final ORB orb = ORB.init(args, p);
         System.out.println("ORB initialized");
 
         final BlockingQueue<Message> incomingMessages = new LinkedBlockingQueue<Message>();
